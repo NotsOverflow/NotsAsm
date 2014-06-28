cf668fc452b812c80e8071122d6d8a8d9f9ecb0e
notsgnik <labruillere@gmail.com>
2014-06-28 19:24:31 +0200

    first commit

---
+++ hello.asm
@@ -0,0 +1,31 @@
+;----------------------------------------------------------------------------|\
+                                                              _____           |                \
+      Autor: Notsgnik                                       /||   /           |          \
+      Email: Labruillere gmail.com                         / ||  /            |         \
+      website: notsgnik.github.io                         /  || /             |         \
+      License: GPL v3                                    /___||/              |           \
+      																		  |                     \
+------------------------------------------------------------------------------
+%define _LINUX64_
+
+%include	'ncam/core.inc'
+	 
+
+@@start
+
+	gchars	hello, {"ecco the dolphin",0Ah}
+	gchar	letter, 'y'
+	
+	gchar	charholder
+	
+	emov	[charholder], [letter]
+	printd(charholder)
+	
+	emov	[charholder], 0Ah
+	printd(charholder)
+	
+	printd(hello.len, hello)	
+	
+	gquit
+
+
